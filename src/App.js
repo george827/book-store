3e2b9da1f19533142248babea0de6d4e91f35ed5
george827 <kinyanjuigeorge827@gmail.com>
2023-01-20 17:10:50 +0300

    prepare the setup for bookstore

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, Link } from 'react-router-dom';
+import Books from './components/Books';
+import Categories from './components/Categories';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <nav className="naviagation">
+        <h1>Bookstore CMS</h1>
+        <ul className="nav-links">
+          <li>
+            <Link className="home-link" to="/">Books</Link>
+          </li>
+          <li>
+            <Link className="categories-link" to="/categories">Categories</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Books />} />
+        <Route path="/categories" element={<Categories />} />
+      </Routes>
+    </>
   );
 }
 
