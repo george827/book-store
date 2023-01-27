565b173eade2d051f4c4b683e273deb1388397c4
george827 <kinyanjuigeorge827@gmail.com>
2023-01-27 11:37:12 +0300

    connect to api

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,4 +1,5 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, applyMiddleware } from '@reduxjs/toolkit';
+import thunk from 'redux-thunk';
 import bookReducer from './books/books';
 import categoriesReducer from './categories/categories';
 
@@ -7,6 +8,7 @@ const store = configureStore({
     books: bookReducer,
     categories: categoriesReducer,
   },
-});
+},
+applyMiddleware((thunk)));
 
 export default store;
